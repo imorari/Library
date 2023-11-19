f63c80a86bb177e6af3355059b87b1d9e4b38fe8
imorari <iulian.morari1995@gmail.com>
2023-11-19 08:58:38 -0500

    need to add read not read functionality and logic and should be done after

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 // All book objects will be stored in this array
-let myLibrary = [
-    
-// {
-// title: "Ephemeral Echoes",
-// author: "Cassandra Nightingale",
-// pages: 348,
-// status: false
-// },
-// {
-// title: "Whispers of the Cosmos",
-// author: "Xavier Stardust",
-// pages: 512,
-// status: true
-// }
-];           
-
+let myLibrary = [];           
 //Book constructor
 
 function Book(title = "Untitled", author = "Unknown Author", pages = 0, status = false) {
@@ -23,7 +8,6 @@ function Book(title = "Untitled", author = "Unknown Author", pages = 0, status =
     this.pages = pages;
     this.status = status;
 }
-
 const newBook1 = new Book("The Hidden Garden", "Eleanor Green", 200, false);
 const newBook2 = new Book("Timeless Odyssey", "Samuel Journey", 350, true);
 const newBook3 = new Book("City of Dreams", "Ava Dreamer", 280, false);
@@ -40,30 +24,11 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // HTML Stuff
 const formContainer = document.querySelector("#form-container");
 
 //Form Creation
-
 function createForm(){
-  
-
-
 const form = document.createElement('form');
 form.id = "form" ;
 form.method = "post";
@@ -110,10 +75,6 @@ submitButton.setAttribute("type", "submit")
 const divTitle = document.createElement("div");
 divTitle.id = "form-title";
 divTitle.innerHTML =`New Book<div class="form-close" id="close"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>close</title><path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" /></svg></div>`
-
-
-
-
 formContainer.classList.add('form-class');
 form.appendChild(divTitle);
 
@@ -136,16 +97,24 @@ inputGroup3.appendChild(labelPages);
 inputGroup3.appendChild(inputPages);
 form.appendChild(inputGroup3)
 
+const switchLabel = document.createElement("label");
+switchLabel.classList.add("switch");
+const checkboxInput = document.createElement("input");
+checkboxInput.setAttribute("type", "checkbox");
+const sliderSpan = document.createElement("span");
+sliderSpan.classList.add("slider");
+sliderSpan.classList.add("round");
+switchLabel.appendChild(checkboxInput);
+switchLabel.appendChild(sliderSpan);
+form.appendChild(switchLabel);
+
 form.appendChild(submitButton)
 formContainer.appendChild(form);
 }
 
 //Card Creation
+const cardPlace = document.querySelector('#main');    //made this public and accessible for other functions
 function createCard(title, author, pages, status){
-
-
-const cardPlace = document.querySelector('#main');
-
 const card = document.createElement("div");
 card.classList.add("card");
 
@@ -185,13 +154,11 @@ cardContent.appendChild(bookTitle);
 cardContent.appendChild(bookAuthor);
 cardContent.appendChild(bookPages);
 cardContent.appendChild(readStatus);
-
 const buttonsContainer = document.createElement("div");
 buttonsContainer.classList.add("card-buttons");
 buttonsContainer.innerHTML = `
-<button id = "read"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>checkbox-marked-circle-outline</title><path d="M20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4C12.76,4 13.5,4.11 14.2,4.31L15.77,2.74C14.61,2.26 13.34,2 12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12M7.91,10.08L6.5,11.5L11,16L21,6L19.59,4.58L11,13.17L7.91,10.08Z" /></svg>Mark as Read</button>  
-<button id = "delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg> Delete</button>
-
+<button id = "read"> <svg class="read" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>checkbox-marked-circle-outline</title><path d="M20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4C12.76,4 13.5,4.11 14.2,4.31L15.77,2.74C14.61,2.26 13.34,2 12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12M7.91,10.08L6.5,11.5L11,16L21,6L19.59,4.58L11,13.17L7.91,10.08Z" /></svg>Mark as Read</button>  
+<button id = "delete"> Delete<svg class = "delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg></button>
 `;
 titleSpan.textContent = title;
 authorSpan.textContent = author;
@@ -200,9 +167,6 @@ readSpan.textContent = status;
 card.appendChild(cardContent);
 card.appendChild(buttonsContainer);
 cardPlace.appendChild(card);
-
-
-
 }
 function clickHandler() {
   createForm();
@@ -220,7 +184,6 @@ form.addEventListener('submit',function(event){
     alert("Please fill in all fields");
     return;
   }
-
   book = new Book(title.value, author.value, pages.value , "temp value")
   addBookToLibrary(book);
   createCard(title.value, author.value, pages.value , "temp value")
@@ -246,38 +209,37 @@ addBook.addEventListener('click', clickHandler);
 for (let i = 0; i < myLibrary.length; i++){
     createCard(myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].readStatus);
 }
-// Card Delete event listener and function
-const deleteButton = document.querySelectorAll("#delete");
-const main = document.querySelector("#main");
-const card = document.querySelector(".card");
-deleteButton.forEach((button) => button.addEventListener('click', deleteBook));
-function deleteBook(){
-    const card = this.parentElement.parentElement;
+
+
+// const cardButtons = document.querySelector(".card-buttons");
+
+cardPlace.addEventListener('click',event=>{
+  if (event.target.id === 'delete'){
+    console.log(event.target.id)
+    const card = event.target.parentElement.parentElement;
     const titleElement = card.querySelector('.title span');
     const title = titleElement.textContent;
       if (card) {
         myLibrary = myLibrary.filter(book => book.title != title);
         card.parentNode.removeChild(card);
       }
-}
-//Card Read button event listener and function
-const readButton = document.querySelectorAll("#read");
-readButton.forEach((button)=>button.addEventListener('click', readFunction))
-function readFunction(){
-    const card = this.parentElement.parentElement;
-    const computedStyle = window.getComputedStyle(this);
-    let backgroundColor = computedStyle.backgroundColor;
+    
+  }
+  if (event.target.id === 'read') {
+    const computedStyle = window.getComputedStyle(event.target);
+    const backgroundColor = computedStyle.backgroundColor;
+    const card = event.target.parentElement.parentElement;
     const readSTatusElement = card.querySelector('.status span');
-    console.log(backgroundColor)
-    if (backgroundColor ==="rgb(0, 0, 255)"){
-      this.style.backgroundColor = 'green';
-      readSTatusElement.textContent = "Finished";
+    if (backgroundColor === "rgb(0, 0, 255)") {
+        event.target.style.backgroundColor = 'green';
+        readSTatusElement.textContent = "Finished";
     }
-    if(backgroundColor === "rgb(0, 128, 0)"){
-      this.style.backgroundColor = 'blue';
-      readSTatusElement.textContent = "Not Finished Yet";
+    if (backgroundColor === "rgb(0, 128, 0)") {
+        event.target.style.backgroundColor = 'blue';
+        readSTatusElement.textContent = "Not Finished Yet";
     }
 }
+})
 // Form Drag Function(from w3 school not mine)
 dragElement(document.getElementById("form-container"));
 
